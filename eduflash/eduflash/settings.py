f52dff0600d668fd34839739432f10266a787fe9
Onyenze Chetachi <chetaGod4life@gmail.com>
2025-06-18 21:07:03 +0100

    ok

--- eduflash/eduflash/settings.py
+++ eduflash/eduflash/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#2y=*+o9i69dnwt0&ozzc@=i(1515e09&yn%^c5#c^j(skz+q6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
